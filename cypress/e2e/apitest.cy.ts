6539985a833f3ac839265d1d3aab3fcb82ed96c2
emmabubu <emma.liu@crypto.com>
2022-11-14 11:56:09 +0800

    init project

---
+++ cypress/e2e/apitest.cy.ts
@@ -0,0 +1,10 @@
+describe("api test", () => {
+  const email = "bubuljj@test.com";
+  const password = "123456";
+
+  beforeEach("login app", () => {
+    cy.loginUI(email, password);
+  });
+
+  it("intercept article", () => {});
+});
