1d62ec3c03901bc0a6b4920c982799ba4c11520d
emmabubu <emma.liu@crypto.com>
2022-11-14 14:15:10 +0800

    Update apitest.cy.ts

--- cypress/e2e/apitest.cy.ts
+++ cypress/e2e/apitest.cy.ts
@@ -1,3 +1,5 @@
+var chance = require("chance").Chance();
+
 describe("api test", () => {
   const email = "bubuljj@test.com";
   const password = "123456";
@@ -8,5 +10,13 @@ describe("api test", () => {
 
   it("intercept article", () => {
     cy.visit("/");
+    cy.intercept("POST", "https://api.realworld.io/api/articles/").as(
+      "postArticle"
+    );
+    cy.visit("/editor");
+    cy.get('input[formcontrolname="title"]').type(
+      chance.sentence({ words: 3 })
+    );
+    cy.get("");
   });
 });
